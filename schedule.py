d0d293b6483594a6a85d7a22581cddb494a1fe58
Ash Pedicini <ash.cat.sandbox@gmail.com>
2024-01-19 17:25:31 -0800

    adding schedule

---
+++ schedule.py
@@ -0,0 +1,7 @@
+'''Good morning, you have the following events on your schedule'''
+
+todays_events = ['breakfast', 'lunch', 'code', 'bang head against the wall', 'sleep']
+
+def today():
+    print(f"Good morning, you have the following events on your schedule: {todays_events}")
+    
